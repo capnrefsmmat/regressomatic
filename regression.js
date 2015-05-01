b6899bcac040de71feab5c9bc7d19d707b3b069f
Alex Reinhart <alex@refsmmat.com>
2015-05-01 15:06:09 -0400

    replace inverse normal CDF code with jStat code
    The previous code I found online somewhere, but it appeared to be copied
    from Numerical Recipes, which is not liberally licensed. So the license
    status was unclear. jStat is MIT, so this is kosher.

--- regression.js
+++ regression.js
@@ -375,44 +375,9 @@ function leverage(hat) {
     return hat.diagonal();
 }
 
-// Complementary error function
-// From Numerical Recipes in C 2e p221,
-// as adapted in
-// https://github.com/errcw/gaussian/blob/5cb185f0d7dd8a6ee6385260d032275c080792fe/lib/gaussian.js
-function erfc(x) {
-    var z = Math.abs(x);
-    var t = 1 / (1 + z / 2);
-    var r = t * Math.exp(-z * z - 1.26551223 + t * (1.00002368 +
-            t * (0.37409196 + t * (0.09678418 + t * (-0.18628806 +
-            t * (0.27886807 + t * (-1.13520398 + t * (1.48851587 +
-            t * (-0.82215223 + t * 0.17087277)))))))));
-    return x >= 0 ? r : 2 - r;
-}
-
-// Inverse complementary error function
-// From Numerical Recipes 3e p265
-// as adapted in
-// https://github.com/errcw/gaussian/blob/5cb185f0d7dd8a6ee6385260d032275c080792fe/lib/gaussian.js
-function ierfc(x) {
-    if (x >= 2) { return -100; }
-    if (x <= 0) { return 100; }
-
-    var xx = (x < 1) ? x : 2 - x;
-    var t = Math.sqrt(-2 * Math.log(xx / 2));
-    var r = -0.70711 * ((2.30753 + t * 0.27061) /
-                        (1 + t * (0.99229 + t * 0.04481)) - t);
-
-    for (var j = 0; j < 2; j++) {
-        var err = erfc(r) - xx;
-        r += err / (1.12837916709551257 * Math.exp(-(r * r)) - r * err);
-    }
-
-    return (x < 1) ? r : -r;
-}
-
 // The standard normal inverse cdf.
 function probit(x) {
-    return - Math.sqrt(2) * ierfc(2 * x);
+    return - Math.sqrt(2) * erfcinv(2 * x);
 }
 
 // For data from a standard normal distribution, this returns the expected value
